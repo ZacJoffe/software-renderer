74d337bf80b7c3d7f4f9c7b1e7e9671827ff1294
Zac Joffe <zacharyjoffe@gmail.com>
2023-05-09 22:12:55 -0400

    Pass vectors by const reference to ctor

--- src/Ray.h
+++ src/Ray.h
@@ -6,7 +6,7 @@ struct Ray {
     glm::vec3 e; // start position
     glm::vec3 d; // direction of ray
 
-    Ray(glm::vec3 e, glm::vec3 d) :
+    Ray(const glm::vec3 & e, const glm::vec3 & d) :
         e(e), d(d) {}
 
     glm::vec3 operator()(float t) const {
